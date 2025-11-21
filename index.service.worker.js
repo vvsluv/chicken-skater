930a5e628b1c4b9eb115b5df3bdabffd40ab8715
vvsluv <uuu100500@mail.ru>
2025-11-21 18:36:17 +0300

    add

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763738726|33915148147';
+const CACHE_VERSION = '1763739370|34558796160';
 /** @type {string} */
 const CACHE_PREFIX = 'ChickenSkater-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
